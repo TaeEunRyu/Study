f3691214bb2eecfe3860028034a671145fd8fab1
TaeEunRyu <isde012@naver.com>
2023-01-03 02:06:48 +0900

    1. 파이썬 변수를 이용한 데이터 관리  - 파이썬 변수에 대한 기초  - 2023.01.01 ( 일요일 )

---
+++ Python/Day1.py
@@ -0,0 +1,43 @@
+#파이썬 변수를 사용한 데이터 관리
+
+# 파이썬에서 콘솔에 출력하는 방법
+# print("Hello World!");
+
+# 출력
+# print("Day 1 - Python Print Function");
+# print("Thefunction is declared like this:");
+# print("print('what to print'))");
+
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the " '"+"' + " sign.")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# 입력
+# print(len(input("What is your name? ")));
+
+# 변수
+# 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+# ###################################
+# Write your code below this line 👇
+
+# c = a;
+# a = b;
+# b = c;
+
+# Write your code above this line 👆
+# ###################################
+
+# 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+# Quiz Band Name Generator
+print("Welcom to the Band Name Generator")
+city = input("What's name of the city you grew up in?\n")
+pet_Name = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet_Name)
